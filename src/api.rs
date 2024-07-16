2f07708b61ed7aeff040ddb4d50bf736ed9fd3d6
stevenportkey <steven@portkey.finance>
2024-07-16 16:16:58 +0800

    ci: try adding a trailing 0

--- src/api.rs
+++ src/api.rs
@@ -134,7 +134,7 @@ mod test {
             assert_eq!(VK_LEN, written_size);
 
             // 2. Prove
-            let input = &*b"{\"a\": [\"12\"], \"b\": [\"3\"]}".iter().map(|&b| b as i8).collect::<Vec<i8>>();
+            let input = &*b"{\"a\": [\"12\"], \"b\": [\"3\"]}\0".iter().map(|&b| b as i8).collect::<Vec<i8>>();
             println!("input0: {:?}", input);
             let res = prove_bn254(Some(ctx_ref), input.as_ptr(), buffer.as_mut_ptr(), BUFFER_SIZE as i32);
             println!("res: {:?}", res);
