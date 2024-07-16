017f98b8d3be4348cceecec57c35fac0117b0af2
stevenportkey <steven@portkey.finance>
2024-07-16 15:56:53 +0800

    ci: troubleshoot

--- src/api.rs
+++ src/api.rs
@@ -136,6 +136,7 @@ mod test {
             let input = &*b"{\"a\": [\"12\"], \"b\": [\"3\"]}".iter().map(|&b| b as i8).collect::<Vec<i8>>();
             let _ = prove_bn254(Some(ctx_ref), input.as_ptr(), buffer.as_mut_ptr(), BUFFER_SIZE as i32);
             let output = decode_string(buffer.as_slice());
+            println!("{:?}", output);
             let output = serde_json::from_str::<ProvingOutput>(&output);
             println!("{:?}", output);
             assert!(output.is_ok());
