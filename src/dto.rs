1ab802d0bc24aefc71d18c4881fc64713776f254
stevenportkey <steven@portkey.finance>
2024-07-16 15:52:06 +0800

    Derive Debug

--- src/dto.rs
+++ src/dto.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use crate::proof::RapidSnarkProof;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct ProvingOutput {
     pub public_inputs: Vec<String>,
     pub proof: RapidSnarkProof,
