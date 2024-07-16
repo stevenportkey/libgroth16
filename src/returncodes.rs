e693922ca516262a92b9f2d7020ce35170026f71
stevenportkey <steven@portkey.finance>
2024-07-16 20:48:21 +0800

    Fix error code

--- src/returncodes.rs
+++ src/returncodes.rs
@@ -3,7 +3,7 @@ pub enum ReturnCodes {
     VerificationFailed = 0,
     InvalidContext = -1,
     InvalidInput = -2,
-    VerificationFailedWithError = 3,
+    VerificationFailedWithError = -3,
     SerializationFailed = -4,
     ProvingFailed = -5,
     BufferTooSmall = -0x1000,
